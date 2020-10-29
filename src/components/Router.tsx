d69bf0462f2a6324b5e69ad1b3ceb316bad44638
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-10-30 02:14:27 +0900

    :globe_with_meridians: Add lecture router
    router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
-import Main from "../routes/Main";
+import Main from "../pages/Main";
+import Lecture from "../pages/Lecture";
 
 export default () => (
   <>
     <Router>
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/lecture/:id" component={Lecture} />
         <Redirect path="*" to="/" />
       </Switch>
     </Router>
