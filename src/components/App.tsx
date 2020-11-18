3dae21cb5c88b18e6710f6d2842e1dbb4cb42bed
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-19 00:59:15 +0900

    :triangular_flag_on_post: Reset Root component
    app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
-import Router from "./Router";
+import { Router } from "../routes";
 
 const App: React.FC<{}> = () => {
   return (
