1a4d07c6b9587cb32df5b0ba98bddd1ec9ae1194
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-20 00:18:57 +0900

    :dizzy: Set index file
    molecules

---
+++ src/components/molecules/index.tsx
@@ -0,0 +1,6 @@
+import MenuProfile from "./MenuProfile";
+import CourseList from "./CourseList";
+import NoticeList from "./NoticeList";
+import MySocialList from "./MySocialList";
+
+export { MenuProfile, CourseList, NoticeList, MySocialList };
