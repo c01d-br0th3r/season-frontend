888b37a691608e8368018c8f9a72ee6810b2c425
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-20 00:21:40 +0900

    :art: Import css files
    css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { Router } from "../routes";
+import "../../node_modules/slick-carousel/slick/slick.css";
+import "../../node_modules/slick-carousel/slick/slick-theme.css";
 
 const App: React.FC<{}> = () => {
   return (
