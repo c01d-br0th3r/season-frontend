2288a624787e919be17d4c913fee4c19353eacee
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-19 22:00:57 +0900

    :sparkles: Set focus style in input components
    atoms

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -11,9 +11,13 @@ const StyledInput = styled.input<IProps>`
   width: 100%;
   padding: 8px;
   margin: ${(props) => props.margin && props.margin};
-  border-radius: 8px;
   outline: none;
-  border: 1px solid #c2c2c2;
+  border: none;
+  border-bottom: 1px solid #d2d2d2;
+  &:focus {
+    border: none;
+    outline: 2px solid #de8a9a;
+  }
 `;
 
 const Input: React.FC<IProps> = ({ placeholder, margin, handleChange }) => {
