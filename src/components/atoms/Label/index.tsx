4bbbd8bcde456d0d3b59a9a5cb0c07171a9f6ff1
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-19 22:01:39 +0900

    :iphone: Add width props in label components
    atoms

--- src/components/atoms/Label/index.tsx
+++ src/components/atoms/Label/index.tsx
@@ -7,12 +7,20 @@ interface IProps {
   fontWeight?: string;
   children?: React.ReactNode;
   margin?: string;
+  hexColor?: string;
+  padding?: string;
+  border?: string;
+  width?: string;
 }
 
-const StyledLabel = styled.span<IProps>`
+const StyledLabel = styled.div<IProps>`
   font-weight: ${(props) => (props.fontWeight ? props.fontWeight : "400")};
   font-size: ${(props) => (props.size ? `${props.size}` : "16px")};
   margin: ${(props) => props.margin && props.margin};
+  color: ${(props) => props.hexColor && props.hexColor};
+  padding: ${(props) => props.padding && props.padding};
+  border: ${(props) => props.border && props.border};
+  width: ${(props) => props.width && props.width};
 `;
 
 const Label: React.FC<IProps> = ({
@@ -21,6 +29,10 @@ const Label: React.FC<IProps> = ({
   fontWeight,
   children,
   margin,
+  hexColor,
+  padding,
+  border,
+  width,
 }) => {
   return (
     <StyledLabel
@@ -28,6 +40,10 @@ const Label: React.FC<IProps> = ({
       className={color}
       size={size}
       margin={margin}
+      hexColor={hexColor}
+      padding={padding}
+      border={border}
+      width={width}
     >
       {children}
     </StyledLabel>
