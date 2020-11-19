e70982af5df018ad4a480a188d2d28208b53d99d
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-20 00:25:52 +0900

    :globe_with_meridians: Make routes for new pages
    router

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
 } from "react-router-dom";
-import { Main, Login } from "../pages";
+import {
+  Home,
+  Login,
+  MyPage,
+  Course,
+  Community,
+  CourseDetail,
+  CommunityDetail,
+} from "../pages";
 
 export default () => (
-  <>
+  <Fragment>
     <Router>
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/mypage" component={MyPage} />
+        <Route exact path="/course" component={Course} />
+        <Route exact path="/community" component={Community} />
+        <Route path="/course/:id" component={CourseDetail} />
+        <Route path="/community/:id" component={CommunityDetail} />
+        <Redirect path="*" to="/" />
       </Switch>
     </Router>
-  </>
+  </Fragment>
 );
