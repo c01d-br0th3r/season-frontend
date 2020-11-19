ad7ac3be779ac6fced19d9011628593076b00143
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-20 00:25:31 +0900

    :lock: Set index file for pages
    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
-import Main from "./Main";
+import Home from "./Home/";
 import Login from "./Login";
+import MyPage from "./MyPage";
+import Course from "./Course";
+import Community from "./Community";
+import CourseDetail from "./CourseDetail";
+import CommunityDetail from "./CommunityDetail";
 
-export { Main, Login };
+export {
+  Home,
+  Login,
+  MyPage,
+  Course,
+  Community,
+  CourseDetail,
+  CommunityDetail,
+};
