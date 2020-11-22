4db1429efa4ed3cd03bddfe3ab428ef2631ea315
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-23 03:37:06 +0900

    :dizzy: Set all global styles "important"
    css

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -22,70 +22,70 @@ const GlobalStyles = createGlobalStyle`
         background-color: #f2f2f2;
     }
     .red1 {
-        color: #dc143c
+        color: #dc143c !important;
     }
     .red2 {
-        color: #d43958
+        color: #d43958 !important;
     }
     .red3 {
-        color: #cc4e67
+        color: #cc4e67 !important;
     }
     .red4 {
-        color: #d66b81
+        color: #d66b81 !important;
     }
     .red5 {
-        color: #de8a9a
+        color: #de8a9a !important;
     }
     .red6 {
-        color: #e0adb7
+        color: #e0adb7 !important;
     }
     .red7 {
-        color: #f5d7dd
+        color: #f5d7dd !important;
     }
     .gray {
-        color: #f0f0f0
+        color: #f0f0f0 !important;
     }
     .green1 {
-        color: #a6f6f1
+        color: #a6f6f1 !important;
     }
     .green2 {
-        color: #e8ffff
+        color: #e8ffff !important;
     }
     .b_red1 {
-        background-color: #dc143c
+        background-color: #dc143c !important;
     }
     .b_red2 {
-        background-color: #d43958
+        background-color: #d43958 !important;
     }
     .b_red3 {
-        background-color: #cc4e67
+        background-color: #cc4e67 !important;
     }
     .b_red4 {
-        background-color: #d66b81
+        background-color: #d66b81 !important;
     }
     .b_red5 {
-        background-color: #de8a9a
+        background-color: #de8a9a !important;
     }
     .b_red6 {
-        background-color: #e0adb7
+        background-color: #e0adb7 !important;
     }
     .b_red7 {
-        background-color: #f5d7dd
+        background-color: #f5d7dd !important;
     }
     .b_gray {
-        background-color: #f0f0f0
+        background-color: #f0f0f0 !important;
     }
     .b_green1 {
-        background-color: #a6f6f1
+        background-color: #a6f6f1 !important;
     }
     .b_green2 {
-        background-color: #e8ffff
+        background-color: #e8ffff !important;
     }
     .white {
-        color: #f2f2f2;
+        color: #f2f2f2 !important;;
     }
     .black {
-        color: #121212;
+        color: #121212 !important;;
     }
 `;
 
