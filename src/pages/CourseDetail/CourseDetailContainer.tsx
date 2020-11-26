2e917811b3f970e80b43e2ba2dab4916bac94589
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-11-27 03:42:40 +0900

    :egg: Add dummy data
    dummy

--- src/pages/CourseDetail/CourseDetailContainer.tsx
+++ src/pages/CourseDetail/CourseDetailContainer.tsx
@@ -56,6 +56,33 @@ const lectureList = [
   },
 ];
 
+const assignmentData = [
+  {
+    id: "1",
+    title: "HW1",
+    content:
+      "첨부된 파일에 설명된 Mano CPU 를 Microprogrammed 방식으로 logisim 툴을 사용하여 설계한다.\
+    과제 2에서 제어신호 찾는 과제를 해봤으므로 각 명령어에 대한 마이크로 오퍼레이션에서 필요한\
+    제어 신호 들을 파악할 수 있고, 과제 4 실습을 통하여 sequencer 부분 설계하는 것을 파악했으면\
+    충분히 주어진 시간에 마칠 수 있습니다.\
+    명령어 코드 매핑을 쉽게 할 수 있도록 Register reference 명령어에 대한 encoding 하는 회로와\
+    carry 제어 모듈 회로가 포함되어 있습니다.\
+    Test 프로그램은 교재에 있는 두 수를 곱하는 프로그램을 보여주고 있습니다. 이 프로그램에서 명령어 19개를\
+    전부 테스트 하지는 않지만 CPU 동작을 확인할 수 있습니다.\
+    여러분이 가지고 있는 과제 3 프로그램을 자신이 설계한 CPU를 구현한 회로에서 실행해 보도록 권장합니다.\
+    ",
+    writter: "국형준",
+    time: "2020-11-27",
+  },
+  {
+    id: "2",
+    title: "HW2",
+    content: "hw2",
+    writter: "국형준",
+    time: "2020-11-28",
+  },
+];
+
 const CourseDetailContainer: React.FC<RouteComponentProps<IMatch>> = ({
   match,
   location,
@@ -66,6 +93,7 @@ const CourseDetailContainer: React.FC<RouteComponentProps<IMatch>> = ({
       pathname={location.pathname}
       lectureList={lectureList}
       notice={noticeData}
+      assignmentData={assignmentData}
     />
   );
 };
